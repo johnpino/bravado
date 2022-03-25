d6b3f1b0f5f3eeb1ba4315265dd6fedaa9351385
John Pino <iam@johnpino.me>
2022-03-25 13:10:26 -0500

    Add Profiles component. Add styling.

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="main">
+    <Profiles />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.main {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ components/Profiles.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="profiles">
+    <div class="profiles--search">
+      <div class="input-icon">
+        <input type="text" placeholder="Search..." />
+        <SearchIcon class="icon" />
+      </div>
+    </div>
+    <div>List</div>
+  </div>
+</template>
+
+<script>
+import { SearchIcon } from "@heroicons/vue/outline";
+
+export default {
+  components: {
+    SearchIcon,
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.profiles {
+  width: 30rem;
+  height: 30rem;
+  padding: 1rem;
+  background-color: white;
+
+  &--search {
+    margin-bottom: 2rem;
+  }
+}
+</style>
